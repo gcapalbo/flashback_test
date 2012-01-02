bb351dba6c5ca5257286a1781ca978662b97118d
Giovanni Capalbo <gcapalbo@dimagi.com>
2012-01-02 17:10:05 -0500

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup (
+    name = "flashback_test"
+   ,version = "1.0"
+   ,packages = ["flashback_test"]
+   ,url = "git://github.com/gcapalbo/flashback_test.git"
+)
+
